44c9bdaa9a46b3c4a5b9e3de89b587678c725a1f
Laoboat People <laoboatpeople@gmail.com>
2026-07-15 10:27:10 -0400

    chore: gitignore __pycache__ and more stray scripts

--- server/count_en.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const p = new PrismaClient();
-p.question.count({where:{locale:"en"}}).then(c => { console.log("EN questions in DB:", c); p.$disconnect(); });
